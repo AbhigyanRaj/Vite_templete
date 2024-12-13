de43c38115eaa685102b2f9507c2945fc1fb03ee
ABHIGYAN <155881024+AbhigyanRaj@users.noreply.github.com>
2024-12-14 00:05:49 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-screen w-full flex items-center justify-center bg-black text-lime-400">
+        <h1>Hii!! I am Abhigyann!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
